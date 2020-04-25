29b70d681b61843def2e4659ea4b0a08dacfe2e5
Sergey Golovnev <golovnevs@gmail.com>
2020-04-25 12:17:28 +0200

    06_homework_online_store_currency_in_array

--- 06_homework_online_store/script.js
+++ 06_homework_online_store/script.js
@@ -4,10 +4,12 @@ let bikes = [{
     engineCapacity: 980,
     colour: "white",
     imgSrc: 'images/bmw.jpg',
-    price: "2500 Euro",
+    price: 2500,
+    currency: "Euro",
     condition: {
         year: 1993,
-        mileage: "40000 km"
+        mileage: 40000,
+        mileageUnit: "km"
     },
     inStock: true,
     description: "BMW motorcycle series, using a two-cylinder four-stroke boxer engine of 980 cc capacity. " +
@@ -20,15 +22,17 @@ let bikes = [{
         engineCapacity: 408,
         colour: "grey",
         imgSrc: 'images/honda.jpg',
-        price: "2500 Euro",
+        price: 2500,
+        currency: "Euro",
         condition: {
             year: 1974,
-            mileage: "32000 km"
+            mileage: 32000,
+            mileageUnit: "km"
         },
         inStock: true,
         description: "The Honda CB400F is a motorcycle produced by Honda from 1975 to 1977. " +
                      "It first appeared at the 1974 Cologne motorcycle show, Intermot, and was dropped from the Honda range in 1978." +
-              "It had an air-cooled, transverse-mounted 408 cc inline four-cylinder engine"
+                     "It had an air-cooled, transverse-mounted 408 cc inline four-cylinder engine"
     },
     {
         brand: "Yamaha",
@@ -36,10 +40,13 @@ let bikes = [{
         engineCapacity: 400,
         colour: "black",
         imgSrc: 'images/yamaha.jpg',
-        price:  "3500 Euro",
+        price:  3500,
+        currency: "Euro",
         condition: {
             year: 1997,
-            mileage: "23000 km"
+            mileage: 23000,
+            mileageUnit: "km"
+
         },
         inStock: true,
         description: "Single-cylinder, air-cooled, two-passenger motorcycles manufactured as a street version of the " +
@@ -52,10 +59,12 @@ let bikes = [{
         engineCapacity: 650,
         colour: "black",
         imgSrc: 'images/kawasaki.jpg',
-        price: "3200 Euro",
+        price: 3200,
+        currency: "Euro",
         condition: {
             year: 2003,
-            mileage: "13000 km"
+            mileage: 13000,
+            mileageUnit: "km"
         },
         inStock: true,
         description: "It is a retro standard motorcycle marketed by Kawasaki for model years 1999-2007" +
@@ -67,10 +76,12 @@ let bikes = [{
         engineCapacity: 865,
         colour: "black",
         imgSrc: 'images/triumph.jpg',
-        price: "6000 Euro",
+        price: 6000,
+        currency: "Pounds",
         condition: {
             year: 2008,
-            mileage: "8000 km"
+            mileage: 8000,
+            mileageUnit: "mi"
         },
         inStock: false,
         description: "The Triumph Thruxton is a series of British motorcycles with parallel-twin engines and sports styling." +
@@ -83,18 +94,18 @@ function bikeDisplay() {
     for (let i = 0; i < bikes.length; i++) {
         bikeCard = document.createElement("div");
         bikeCard.setAttribute("class", "card");
-        const bikeEl = document.getElementById('card');
-        bikeCard.innerHTML = `
+        const main = document.getElementById('main')
+        bikeCard.innerHTML = ` 
           <h2>${bikes[i].brand} ${bikes[i].model}</h2>
           <img class="bikePic" src="${bikes[i].imgSrc}" />
-          <li>Price: ${bikes[i].price}</li>
+          <li>Price: ${bikes[i].price} ${bikes[i].currency}</li>
           <li>Year: ${bikes[i].condition.year}</li>
-          <li>Mileage: ${bikes[i].condition.mileage}</li>
+          <li>Mileage: ${bikes[i].condition.mileage} ${bikes[i].condition.mileageUnit}</li>
           <li>Colour: ${bikes[i].colour}</li>
           <li>Engine: ${bikes[i].engineCapacity} cc</li>
           <p>${bikes[i].description}</p>
           <button class="buyButton"> Buy now </button>
-                            `;
+        `;
         main.appendChild(bikeCard);
     }
 }
@@ -103,7 +114,7 @@ function bikesCountDisplay() {
     bikeCountEl = document.getElementById('welcomeTitle');
     bikeCountEl.innerHTML = `
     Look at these ${bikes.length} awesome bikes below:
-                            `;
+    `;
 }
 bikesCountDisplay();
 bikeDisplay();
